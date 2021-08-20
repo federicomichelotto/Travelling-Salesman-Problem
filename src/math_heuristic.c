fb3586de79764ccf521e1c6c2fa003a7011e2995
federicomichelottounipd <80097961+federicomichelottounipd@users.noreply.github.com>
2021-08-20 17:03:59 +0200

    changes in hard fixing heuristic: added additional constraints

--- src/math_heuristic.c
+++ src/math_heuristic.c
@@ -76,7 +76,7 @@ int math_solver(instance *inst)
     if (CPXgetx(env, lp, inst->best_sol, 0, inst->cols - 1))
         print_error("CPXgetx() error");
 
-    CPXgetobjval(env, lp, &inst->z_best);      // Best objective value
+    CPXgetobjval(env, lp, &inst->z_best); // Best objective value
     printf("\tIncumbent: %f\n\n", inst->z_best);
     gather_solution(inst, inst->best_sol, 0);
     save_and_plot_solution(inst, 0);
@@ -91,7 +91,7 @@ int math_solver(instance *inst)
             printf("Initial incumbent: %f\n\n", inst->z_best);
         if (CPXsetintparam(env, CPX_PARAM_NODELIM, 2100000000))
             print_error("CPX_PARAM_NODELIM error");
-        hard_fixing_heuristic(env, lp, inst, (int)inst->time_limit / 20, 0.8);
+        hard_fixing_heuristic(env, lp, inst, (int)inst->time_limit / 20, 0.9);
     }
     // soft-fixing heuristic
     else if (inst->model_type == 1)
@@ -122,6 +122,7 @@ int math_solver(instance *inst)
     return 0;
 }
 
+// fix_ratio: probability to fix and edge
 void hard_fixing_heuristic(CPXENVptr env, CPXLPptr lp, instance *inst, int time_limit_iter, double fix_ratio)
 {
     int iter = 1;
@@ -129,9 +130,24 @@ void hard_fixing_heuristic(CPXENVptr env, CPXLPptr lp, instance *inst, int time_
     CPXgetdblparam(env, CPX_PARAM_EPGAP, &gap);
     double ts_last_impr; // timestamp last improvment
     inst->param.ticks ? CPXgetdettime(env, &ts_last_impr) : getTimeStamp(&ts_last_impr);
+    int not_improved = 0; // #consecutive iterations in which the incumbent does not improve
+
+    int *indices_additional = (int *)malloc(inst->dimension * sizeof(int));
+    double *values_additional = (double *)malloc(inst->dimension * sizeof(double));
+    char *senses_additional = (char *)malloc(inst->dimension * sizeof(char)); // we only need to change the lower bound of our variables
+    int k = 0;
 
     while (1)
     {
+        //printf("\n[ITERATION %d]:\n best tour so far: ", iter);
+        int node = 0;
+        // printf("%d", node);
+        // for (int i = 0; i < inst->dimension; i++)
+        // {
+        //     printf(" -> %d", inst->succ[node]);
+        //     node = inst->succ[node];
+        // }
+        // printf("\n");
         // update time left
         double ts_current;
         inst->param.ticks ? CPXgetdettime(env, &ts_current) : getTimeStamp(&ts_current);
@@ -140,33 +156,90 @@ void hard_fixing_heuristic(CPXENVptr env, CPXLPptr lp, instance *inst, int time_
             return;
         if (time_left < time_limit_iter)
             CPXsetdblparam(env, CPX_PARAM_TILIM, time_left);
-        // if (iter % 100 == 0)
-        // {
-        //     if (inst->param.ticks)
-        //         printf("[Iteration %d] time left %f ticks\n", iter, time_left);
-        //     else
-        //         printf("[Iteration %d] time left %f seconds\n", iter, time_left);
-        // }
-        // allocate two arrays with size ncols
-        int *indices = (int *)malloc(inst->cols * sizeof(int));
-        double *values = (double *)malloc(inst->cols * sizeof(double));
-        char senses[inst->cols]; // we only need to change the lower bound of our variables
-        int nedges = inst->dimension * (inst->dimension - 1) / 2;
-        for (int k = 0; k < nedges; k++)
+
+        // restore the upper bounds previusly modified (additional)
+        if (iter > 1)
         {
-            indices[k] = k;
-            values[k] = 0.0;
-            senses[k] = 'L';
-            // random selection of a subset of arcs of the best solution found so far
-            //printf("*** best_sol[%d] = %f\n",k,inst->best_sol[k]);
-            if (inst->best_sol[k] > 0.5)
-                if ((rand() % 100) + 1 < fix_ratio * 100)
-                    values[k] = 1.0;
+            for (int i = 0; i < k; i++)
+                values_additional[i] = 1.0;
+
+            if (CPXchgbds(env, lp, k, indices_additional, senses_additional, values_additional))
+                print_error("CPXchgbds hard-fixing upper bound error");
+        }
+
+        if (CPXchgbds(env, lp, k, indices_additional, senses_additional, values_additional))
+            print_error("CPXchgbds hard-fixing upper bound error");
+
+        int *indices = (int *)malloc(inst->dimension * sizeof(int));
+        double *values = (double *)malloc(inst->dimension * sizeof(double));
+        char *senses = (char *)malloc(inst->dimension * sizeof(char)); // we only need to change the lower bound of our variables
+
+        // the best solution so far is also stored in the successor format in the inst->succ array
+        node = 0;
+        int close_node = 0, flag_start = 0;
+        int start_node = -1;
+        int keep_counter = 0;
+        k = 0;
+
+        for (int i = 0; i < inst->dimension; i++)
+        {
+            indices[i] = xpos(node, inst->succ[node], inst);
+            senses[i] = 'L';
+            values[i] = 1.0;
+            // decide randomly if keep fixed the edge (i,succ[i]) or not
+            if ((rand() % 100) <= (1 - fix_ratio) * 100) // don't keep the edge
+            {
+                if (flag_start == 0)
+                    flag_start = 1;
+
+                values[i] = 0.0; // now the edge (i,succ[i]) is not fixed
+
+                if (keep_counter > 0) // there is a path of [keep_counter] edges that ends in [node]
+                {
+                    keep_counter = 0;
+                    if (inst->succ[start_node] != node) // the path must have AT LEAST 2 edges
+                    {
+                        // deselect the edge (start_node, node)
+                        indices_additional[k] = xpos(start_node, node, inst);
+                        senses_additional[k] = 'U';
+                        values_additional[k++] = 0.0;
+                        //printf("*ub[%d,%d] = 0.0\n", start_node, node);
+                    }
+                }
+                //printf("lb[%d,%d] = 0.0\n", node, inst->succ[node]);
+            }
+            else // keep the edge
+            {
+                if (flag_start == 0)
+                {
+                    close_node = inst->succ[node];
+                }
+                else
+                {
+                    if (keep_counter == 0)
+                        start_node = node;
+                    keep_counter++;
+                }
+                //printf("lb[%d,%d] = 1.0\n", node, inst->succ[node]);
+            }
+            node = inst->succ[node];
+        }
+        if (close_node > 0 && keep_counter > 0) // there is a sequence of [keep_counter+close_node] selected edges that pass through from the node 0
+        {
+            indices_additional[k] = xpos(start_node, close_node, inst);
+            senses_additional[k] = 'U';
+            values_additional[k++] = 0.0;
+            //printf("**ub[%d,%d] = 0.0\n", start_node, close_node);
         }
 
         // change the lower bounds
-        if (CPXchgbds(env, lp, nedges, indices, senses, values))
-            print_error("CPXchgbds error");
+        if (CPXchgbds(env, lp, inst->dimension, indices, senses, values))
+            print_error("CPXchgbds hard-fixing lower bound error");
+
+        // change the upper bounds (additional)
+        if (CPXchgbds(env, lp, k, indices_additional, senses_additional, values_additional))
+            print_error("CPXchgbds hard-fixing upper bound error");
+        //CPXwriteprob(env, lp, "prova.lp", NULL);
 
         // solve with the new constraints
         if (CPXmipopt(env, lp))
@@ -195,27 +268,43 @@ void hard_fixing_heuristic(CPXENVptr env, CPXLPptr lp, instance *inst, int time_
             }
             gather_solution(inst, inst->best_sol, 0);
             save_and_plot_solution(inst, iter);
+            // for (int i = 0; i < inst->dimension; i++)
+            //     printf("x[%d,%d] = 1.0\n", i, inst->succ[i]);
 
             int beg = 0;
-            if (CPXaddmipstarts(env, lp, 1, nedges, &beg, indices, values, CPX_MIPSTART_AUTO, NULL))
+            if (CPXaddmipstarts(env, lp, 1, inst->dimension, &beg, indices, values, CPX_MIPSTART_AUTO, NULL))
                 print_error("CPXaddmipstarts error");
+            not_improved = 0;
         }
         else
         {
             if (ts_current - ts_last_impr > 10)
             {
                 if (inst->param.ticks)
-                    printf("[Iteration %d] Incumbent not improved, time left %f ticks\n", iter, time_left);
+                    printf("[Iteration %d] Incumbent not improved, time left = %f ticks\n", iter, time_left);
                 else
-                    printf("[Iteration %d] Incumbent not improved, time left %f seconds\n", iter, time_left);
+                    printf("[Iteration %d] Incumbent not improved, time left = %f seconds\n", iter, time_left);
                 ts_last_impr = ts_current;
+            }
+            if (not_improved++ > 10)
+            {
+                if (fix_ratio >= 0.55)
+                {
+                    fix_ratio -= 0.05;
+                    printf("[Iteration %d] new fix ratio: %f\n", iter, fix_ratio);
+                    not_improved = 0;
+                }
             }
         }
 
         iter++;
         free(indices);
+        free(senses);
         free(values);
     }
+    free(indices_additional);
+    free(senses_additional);
+    free(values_additional);
 }
 
 void soft_fixing_heuristic(CPXENVptr env, CPXLPptr lp, instance *inst, int time_limit_iter)
@@ -305,9 +394,9 @@ void soft_fixing_heuristic(CPXENVptr env, CPXLPptr lp, instance *inst, int time_
                 return;
             }
             if (inst->param.ticks)
-                printf("[Iteration %d] Incumbent not improved, k = %d, time left %f ticks\n", iter, k, time_left);
+                printf("[Iteration %d] Incumbent not improved, k = %d, time left = %f ticks\n", iter, k, time_left);
             else
-                printf("[Iteration %d] Incumbent not improved, k = %d, time left %f seconds\n", iter, k, time_left);
+                printf("[Iteration %d] Incumbent not improved, k = %d, time left = %f seconds\n", iter, k, time_left);
         }
         if (CPXdelrows(env, lp, row, row))
             print_error("CPXdelrows error");
