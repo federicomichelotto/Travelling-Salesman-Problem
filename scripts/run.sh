439decb3928ad1d97462e49089b43a28f982c532
Mikele Milia <mikele.milia@studenti.unipd.it>
2021-08-21 09:50:36 +0200

    Minor fix deprecation error cluster

--- scripts/run.sh
+++ scripts/run.sh
@@ -2,26 +2,35 @@
 
 # Local variable
 #SOLVER=("comp" "iter" "math" "heur" "tabu" "gene")
-SOLVER=("tabu" "gene")
-GENETIC=("gen00" "gen01" "gen10" "gen11" "gen20" "gen21" "gen30" "gen31")
+#GENETIC=("gen00" "gen01" "gen10" "gen11" "gen20" "gen21" "gen30" "gen31")
+SOLVER=("comp" "iter" "math")
 
 # Move into build folder
 for solver in "${SOLVER[@]}"; do
 
   if [ "$solver" == "comp" ]; then
+    echo "Compact model submitted"
     for m in {1,2,5,8}; do
-      sbatch "$solver"-32-tsp.slurm -m $m
+      sbatch "$solver"32-tsp.slurm -m $m
     done
 
     for m in {3,4,6,7}; do
-      sbatch "$solver"-64-tsp.slurm -m $m
+      sbatch "$solver"64-tsp.slurm -m $m
     done
 
   elif [ "$solver" == "iter" ]; then
-    sbatch "$solver"-tsp.slurm
+    echo "Iterative model submitted"
+
+    for m in {9,10,11}; do
+      sbatch "$solver"-tsp.slurm -m $m
+    done
 
   elif [ "$solver" == "math" ]; then
-    sbatch "$solver"-tsp.slurm
+    echo "Matheuristic model submitted"
+
+    for m in {0,1}; do
+      sbatch "$solver"-tsp.slurm -m $m
+    done
 
   elif [ "$solver" == "heur" ]; then
     sbatch "$solver"-tsp.slurm
